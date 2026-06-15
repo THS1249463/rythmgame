3d031e043435855dd50f5fb30e137ad1d8113933
THS <hs.tsai22@gmail.com>
2026-06-15 12:55:05 +0800

    已大致完成我的目標可以demo的樣子!還請cookie看看~

--- Assets/Script/cookie/spawn_new.cs
+++ Assets/Script/cookie/spawn_new.cs
@@ -11,7 +11,7 @@ public class spawn_new : MonoBehaviour
 
     public updatecombo uc;
     public AudioSource audioSource;
-    public AudioClip susSong;
+    public AudioClip susSong,susSongshort,spearofjustice;
     public int misscount = 0, critcount = 0, combo = 0, maxcombo = 0;
     public bool playingstatus = false;
     public title tt;
@@ -36,11 +36,27 @@ public class spawn_new : MonoBehaviour
 
         }
 
-        if ((Input.GetKeyDown(KeyCode.Alpha2)) && !playingstatus)
+        if ((Input.GetKeyDown(KeyCode.Alpha4)) && !playingstatus)
         {
             playingstatus = true;
             StartCoroutine(amongus());
             Invoke("SusSongPlay", 0.8f);
+
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Alpha3)) && !playingstatus)
+        {
+            playingstatus = true;
+            StartCoroutine(undyne());
+            Invoke("playundyne", 0.9f);
+
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Alpha2)) && !playingstatus)
+        {
+            playingstatus = true;
+            StartCoroutine(amongusshort());
+            Invoke("SusSongPlayShort", 0.9f);
 
         }
     }
@@ -75,6 +91,49 @@ public class spawn_new : MonoBehaviour
     // Point 2 =中 (10, 0)
     // Point 3 =下 (10, 2) / (2, 2) / (0, 10) 類型的變化點
     // Point 4 =右 (10, -2) / (0, -10) 類型的變化點
+
+    IEnumerator amongusshort()
+    {
+        tt.hide();
+        ResetStats();
+
+        ShootNote(0, 2);
+        yield return new WaitForSeconds(0.6f);
+
+        ShootNote(1, 0);
+        yield return new WaitForSeconds(0.3f);
+        ShootNote(2, 1);
+        yield return new WaitForSeconds(0.3f);
+        ShootNote(3, 4);
+        yield return new WaitForSeconds(0.3f);
+        ShootNote(4, 3);
+        yield return new WaitForSeconds(0.3f);
+        ShootNote(5, 0);
+        yield return new WaitForSeconds(0.3f);
+        ShootNote(6, 2);
+        yield return new WaitForSeconds(0.3f);
+        ShootNote(7, 4);
+        yield return new WaitForSeconds(0.1f);
+
+        yield return new WaitForSeconds(0.8f);
+        ShootNote(10, 1);
+        yield return new WaitForSeconds(0.15f);
+        ShootNote(0, 2);
+        yield return new WaitForSeconds(0.15f);
+        ShootNote(1, 3);
+        yield return new WaitForSeconds(1f);
+        //
+
+        yield return new WaitForSeconds(1f);
+        Debug.Log("CRIT:" + critcount + "/MISS:" + misscount + "/Combo:" + maxcombo);
+        uc.setEndText();
+        yield return new WaitUntil(
+            () => Input.GetKeyDown(KeyCode.Return)
+        );
+        audioSource.Stop();
+        tt.appear();
+        playingstatus = false;
+    }
 
     IEnumerator amongus()
     {
@@ -240,6 +299,145 @@ public class spawn_new : MonoBehaviour
         playingstatus = false;
     }
 
+    IEnumerator undyne()
+    {
+        tt.hide();
+        ResetStats();
+
+        ShootNote(0, 2);
+        yield return new WaitForSeconds(1.35f);
+        ShootNote(1, 0);
+        yield return new WaitForSeconds(1.35f);
+        ShootNote(2, 4);
+        yield return new WaitForSeconds(1.35f);
+        ShootNote(3, 1);
+        yield return new WaitForSeconds(0.7f);
+        ShootNote(4, 3);
+        yield return new WaitForSeconds(0.7f);
+        //
+        ShootNote(5, 2);
+        yield return new WaitForSeconds(1.35f);
+        ShootNote(6, 4);
+        yield return new WaitForSeconds(1.35f);
+        ShootNote(7, 3);
+        yield return new WaitForSeconds(1.35f);
+        ShootNote(8, 0);
+        yield return new WaitForSeconds(0.7f);
+        ShootNote(9, 1);
+        yield return new WaitForSeconds(0.7f);
+        //
+        ShootNote(6, 2);
+        yield return new WaitForSeconds(1.35f-0.5f);
+        ShootNote(1, 0);
+        yield return new WaitForSeconds(0.5f);
+        ShootNote(7, 2);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(2, 4);
+        yield return new WaitForSeconds(0.5f);
+        ShootNote(8, 2);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(3, 1);
+        yield return new WaitForSeconds(0.5f);
+        ShootNote(9, 2);
+        yield return new WaitForSeconds(0.7f);
+        ShootNote(10, 3);
+        yield return new WaitForSeconds(0.7f);
+        //
+        ShootNote(6, 2);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(1, 3);
+        yield return new WaitForSeconds(0.5f);
+        ShootNote(7, 2);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(2, 1);
+        yield return new WaitForSeconds(0.5f);
+        ShootNote(8, 2);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(3, 4);
+        yield return new WaitForSeconds(0.5f);
+        ShootNote(9, 2);
+        yield return new WaitForSeconds(0.7f);
+        ShootNote(10, 0);
+        yield return new WaitForSeconds(0.7f);
+        //
+        ShootNote(6, 2);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(1, 1);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(2, 2);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(7, 1);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(3, 3);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(4, 2);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(8, 3);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(5, 1);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(6, 2);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(9, 1);
+        yield return new WaitForSeconds(0.7f);
+        ShootNote(10, 4);
+        yield return new WaitForSeconds(0.7f);
+        //
+        ShootNote(6, 2);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(1, 0);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(2, 2);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(7, 0);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(3, 4);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(4, 2);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(8, 4);
+        yield return new WaitForSeconds(1.35f - 0.5f);
+        ShootNote(5, 0);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(6, 2);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(9, 0);
+        yield return new WaitForSeconds(0.7f);
+        ShootNote(10, 4);
+        yield return new WaitForSeconds(0.7f);
+
+
+        ShootNote(1, 1);
+        yield return new WaitForSeconds(0.7f);
+        ShootNote(2, 3);
+        yield return new WaitForSeconds(0.7f);
+        ShootNote(3, 4);
+        yield return new WaitForSeconds(0.7f);
+        ShootNote(4, 2);
+        yield return new WaitForSeconds(0.7f);
+        //1 2 3 2
+        ShootNote(5, 1);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(6, 2);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(7, 3);
+        yield return new WaitForSeconds(0.25f);
+        ShootNote(8, 2);
+        yield return new WaitForSeconds(0.65f);
+        ShootNote(9, 0);
+        yield return new WaitForSeconds(1f);
+
+        yield return new WaitForSeconds(1f);
+        Debug.Log("CRIT:" + critcount + "/MISS:" + misscount + "/Combo:" + maxcombo);
+        uc.setEndText();
+        yield return new WaitUntil(
+            () => Input.GetKeyDown(KeyCode.Return)
+        );
+        audioSource.Stop();
+        tt.appear();
+        playingstatus = false;
+    }
+
     void ResetStats()
     {
         
@@ -252,13 +450,13 @@ public class spawn_new : MonoBehaviour
         audioSource.PlayOneShot(susSong);
     }
 
-    public void SusSongPlay_0()
+    public void playundyne()
     {
-        audioSource.PlayOneShot(susSong);
+        audioSource.PlayOneShot(spearofjustice);
     }
 
-    public void SusSongPlay_1()
+    public void SusSongPlayShort()
     {
-        audioSource.PlayOneShot(susSong);
+        audioSource.PlayOneShot(susSongshort);
     }
 }
